aa91bcd90afd31c7b3c098e07734e85fd17ff29c
fabriziosciolto <guiltybrizio@gmail.com>
2023-01-12 19:28:47 +0100

    feat: added delete function. fix: fixed some bugs

--- src/bot.py
+++ src/bot.py
@@ -8,31 +8,44 @@ updater=Updater(token, use_context=True)
 my_list=list()
 
 def start(update, context):
-    update.message.reply_text("Ciao! Sono Shopping List Bot, il tuo assistente per la spesa.\n")
+    update.message.reply_text("Ciao! Sono Shopping List Bot, il tuo assistente per la spesa\n")
 
 def help(update, context):
-    update.message.reply_text("Comandi:\n/add per aggiungere un prodotto\n/show per visualizzare la lista\n/check per spuntare un elemento dalla lista\n/delete per svuotere la lista\n")
+    update.message.reply_text("Comandi:\n/add articolo  ->  inserisce l'articolo\n/remove articolo  ->  elimina l'articolo\n/show  ->  mostra la lista\n/delete  ->  svuota la lista\n")
 
 def unknown(update, context):
     update.message.reply_text("Comando non valido, scrivi /help per conoscere la lista di comandi\n")
 
 def add(update, context):
-    x=context.args[0]
-    #concatenare in x le varie stringe di args con il +
+    x=""
+    for y in context.args:
+        x+=(y+" ")
     if x in my_list:
-        update.message.reply_text("Il prodotto è già in lista.\n")
+        update.message.reply_text("Il prodotto è già in lista\n")
     else:
         my_list.append(x)
-    #si deve correggere la lettura di args per elementi formati da più parole
+        update.message.reply_text(x+" aggiunto alla lista\n")
+
+def remove(update, context):
+    x=""
+    for y in context.args:
+        x+=(y+" ")
+    if x in my_list:
+        my_list.remove(x)
+        update.message.reply_text(x+" rimosso dalla lista\n")
+    else:
+        update.message.reply_text("Il prodotto non è in lista\n")
 
 def show(update, context):
-    update.message.reply_text('Lista:\n- '+'\n- '.join(my_list))
-    #x=1
-    #update.message.reply_text(str(x)+'. '+('\n'+str(x)+'. ').join(my_list))
+    if not my_list:
+        update.message.reply_text("La lista è vuota\n")
+    else:
+        update.message.reply_text('Lista:\n- '+'\n- '.join(my_list))
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('help', help))
 updater.dispatcher.add_handler(CommandHandler('add', add, pass_args=True))
+updater.dispatcher.add_handler(CommandHandler('remove', remove, pass_args=True))
 updater.dispatcher.add_handler(CommandHandler('show', show))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
 updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown))
